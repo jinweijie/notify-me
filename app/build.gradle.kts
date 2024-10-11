d7190ff7a1c379f92948387d9a064dab5dee59e1
JIN Weijie <jinweijie@gmail.com>
2024-10-11 23:09:55 +0800

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.1.2"
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
